19bd43e48a87f5a4b3153f2a5df055121c5aca72
jscOptimov <jonas@optimov.com>
2014-05-21 02:58:59 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+//var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server = app.listen(3000);
+//server.listen(app.get('port'), function(){
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
+
+// all environments
+//app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function(req, res) {
+  res.render('index', {title:'Welcome tho!'});
+});
+app.get('/testpage', function(req, res) {
+  res.render('index', {title: 'TEST'});
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+  socket.emit('news', {sup: 'heyoo'});
+});
+
+console.log('Express listening on port 3000');
