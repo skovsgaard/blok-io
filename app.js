82e0587e36ba31a65ce18232db63db38585ebbcd
jscOptimov <jonas@optimov.com>
2014-05-21 14:32:14 +0200

    Commented out code removed

--- app.js
+++ app.js
@@ -4,18 +4,14 @@
  */
 
 var express = require('express');
-//var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var level = require('level');
 
+var db = level();
 var app = express();
 var server = app.listen(3000);
-//server.listen(app.get('port'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
-//});
 
-// all environments
-//app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -31,18 +27,21 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Route and render the base path
 app.get('/', function(req, res) {
   res.render('index', {title:'Welcome tho!'});
 });
 
+// Route and render the admin interface
 app.get('/admin', function(req, res) {
   res.render('admin', {title: 'the admin'});
   io.socket.removeAllListeners('connection');
 });
 
+// Make a new post from within the admin
 app.post('/postpost', function(req, res){
   res.send("GREAT SUCCESS!\n" + req.body.postBody);
-})
+});
 
 var io = require('socket.io').listen(server);
 
